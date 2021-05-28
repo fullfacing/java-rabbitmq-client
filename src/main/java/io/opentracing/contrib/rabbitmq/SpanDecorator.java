422af3e40e6d13d7022737206dbd03bc7cb7b38a
stuartjameson <stuartjmsn3@gmail.com>
2021-05-28 16:22:14 +0200

    Added all available details to rabbitmq traces

--- src/main/java/io/opentracing/contrib/rabbitmq/SpanDecorator.java
+++ src/main/java/io/opentracing/contrib/rabbitmq/SpanDecorator.java
@@ -14,8 +14,11 @@
 package io.opentracing.contrib.rabbitmq;
 
 
+import com.rabbitmq.client.AMQP;
 import io.opentracing.Span;
 import io.opentracing.tag.Tags;
+
+import java.util.function.Consumer;
 
 public class SpanDecorator {
   public static final String COMPONENT_NAME = "java-rabbitmq";
@@ -27,5 +30,32 @@ public class SpanDecorator {
 
   public static void onResponse(Span span) {
     Tags.COMPONENT.set(span, COMPONENT_NAME);
+  }
+
+  private static <A> void notNull(A value, Consumer<String> action) {
+    if (value != null) action.accept(value.toString());
+  }
+
+  private static void notNull(String value, Consumer<String> action) {
+    if (value != null) action.accept(value);
+  }
+
+  public static void addProperties(Span span, AMQP.BasicProperties properties) {
+    notNull(properties.getUserId(),          v -> span.setTag("user.id", v));
+    notNull(properties.getContentType(),     v -> span.setTag("content.type", v));
+    notNull(properties.getContentEncoding(), v -> span.setTag("content.encoding", v));
+    notNull(properties.getMessageId(),       v -> span.setTag("message.id", v));
+    notNull(properties.getCorrelationId(),   v -> span.setTag("correlation.id", v));
+    notNull(properties.getDeliveryMode(),    v -> span.setTag("delivery.mode", v));
+    notNull(properties.getReplyTo(),         v -> span.setTag("reply.to", v));
+    notNull(properties.getPriority(),        v -> span.setTag("priority", v));
+
+    span.setTag("body.size", properties.getBodySize());
+
+    if (properties.getHeaders() != null) {
+      properties
+              .getHeaders()
+              .forEach((key, value) -> span.setTag("header." + key, value.toString()));
+    }
   }
 }
